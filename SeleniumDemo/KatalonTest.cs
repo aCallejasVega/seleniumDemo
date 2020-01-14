da1ef26242443380976e0df2eb48b16bca9aebf8
Quispe Rodriguez <PQuispe@bnb.com.bo>
2020-01-14 14:06:19 -0400

    Primer comit

---
+++ SeleniumDemo/KatalonTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumDemo
+{
+    class KatalonTest
+    {
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Asfi()
+        {
+            Console.Write("---------------------------------------- Test1 >");
+            IWebDriver driver = new ChromeDriver(@"D:\03.PROYECTOS\01.BNBPUNTOS\PROYECTO REST\DesarrolloBNB");
+
+
+            driver.Navigate().GoToUrl("http://apps.supernet.bo/ic/Autentication.aspx");
+            Thread.Sleep(500);
+            driver.FindElement(By.Id("IFMainContent_txtLogin")).Click();
+            driver.FindElement(By.Id("IFMainContent_txtLogin")).Clear();
+            driver.FindElement(By.Id("IFMainContent_txtLogin")).SendKeys("dmanzano@bnb.com.bo");
+            driver.FindElement(By.Id("IFMainContent_txtPasswd")).Click();
+            driver.FindElement(By.Id("IFMainContent_txtPasswd")).Clear();
+            driver.FindElement(By.Id("IFMainContent_txtPasswd")).SendKeys("Daniela123*");
+            driver.FindElement(By.Id("IFMainContent_btnEntrar")).Click();
+            Thread.Sleep(1000);
+
+            driver.Close();
+           
+        }
+    }
+}
---
+++ SeleniumDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("--------------------------------- Main");
+            KatalonTest a = new KatalonTest();
+            a.Asfi();
+            Console.Write("--------------------------------- Main");
+        }
+    }
+}
